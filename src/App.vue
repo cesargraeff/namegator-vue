c0e0badb24c436d061cc1279e97dcd086d2f79fb
César Augusto Graeff <cesaraugustogff@gmail.com>
2019-10-15 11:49:16 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,151 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator <span class="fa fa-check"></span></h1>
+      <br>
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node.js</h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>Prefixos <span class="badge badge-info">{{prefixes.length}}</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                    <div class="row">
+                      <div class="col-md">
+                        {{ prefix }}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deletePrefix(prefix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+                <br/>
+                <div class="input-group">
+                  <input type="text" class="form-control" v-model="prefix" v-on:keyup.enter="addPrefix(prefix)" placeholder="Digite o prefixo" />
+                  <div class="input-group-append">
+                    <div class="btn btn-info" v-on:click="addPrefix(prefix)"><span class="fa fa-plus"></span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>Sufixos <span class="badge badge-info">{{sufixes.length}}</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                    <div class="row">
+                      <div class="col-md">
+                        {{ sufix }}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deleteSufix(sufix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+                <br/>
+                <div class="input-group">
+                  <input type="text" class="form-control" v-model="sufix" v-on:keyup.enter="addSufix(sufix)" placeholder="Digite o sufixo" />
+                  <div class="input-group-append">
+                    <div class="btn btn-info" v-on:click="addSufix(sufix)"><span class="fa fa-plus"></span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                <div class="row">
+                  <div class="col-md">
+                    {{ domain.name }}
+                  </div>
+                  <div class="col-md text-right">
+                    <a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+                      <span class="fa fa-shopping-cart"></span>
+                    </a>
+                  </div>
+                </div>
+                
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	name: "app",
+	data : function() {
+		return {
+			prefix: "",
+			sufix: "",
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"],
+		};
+	},
+	methods: {
+		addPrefix(prefix){
+			this.prefixes.push(prefix);
+			this.prefix = "";
+		},
+
+		addSufix(sufix){
+			this.sufixes.push(sufix);
+			this.sufix = "";
+		},
+		deletePrefix(prefix){
+			this.prefixes.splice(this.prefixes.indexOf(prefix),1);
+		},
+		deleteSufix(sufix){
+			this.sufixes.splice(this.sufixes.indexOf(sufix),1);
+		},
+	},
+	computed: {
+		domains(){
+			const domains = [];
+			for(const prefix of this.prefixes){
+				for(const sufix  of this.sufixes){
+					const name = prefix+sufix;
+					const url = name.toLowerCase();
+					const checkout = "https://checkout.hostgator.com.br/?a=add&sld="+url+"&tld=.com.br";
+					domains.push({
+						name,
+						checkout
+					});
+				}
+			}
+			return domains;
+		}
+	}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#slogan{
+    margin-top: 30px;
+    margin-bottom: 30px;
+}
+#main{
+  background-color: #F1F1F1;
+  padding: 30px 0;
 }
 </style>
